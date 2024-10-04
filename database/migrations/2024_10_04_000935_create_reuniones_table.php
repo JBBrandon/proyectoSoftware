2e4b1375a8fd426ae40c4be12a6f79bc25a7a1eb
JBBrandon <jbcm.32@gmail.com>
2024-10-03 19:40:01 -0500

    Creasion de base de datos

---
+++ database/migrations/2024_10_04_000935_create_reuniones_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reuniones', function (Blueprint $table) {
+            $table->id();
+            $table->string('idReuniones');
+            $table->string('tutor_id');
+            $table->string('estudiante_id');
+            $table->string('fecha_reunion');
+            $table->string('detalles');
+            $table->string('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reuniones');
+    }
+};
